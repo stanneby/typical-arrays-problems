a17edb46f514f0d73af4a4f399a2c4c15c1ff251
stas <stas.v.nebyshinets@gmail.com>
2021-02-07 17:49:35 +0300

    feat: update min, max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,49 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined){
+    return 0;
+  }
+  let len = array.length;
+  if(len == 0){
+    return 0;
+  }
+  let min =  array[0];
+  for(let i = 1; i < len; i++){
+    if(array[i] < min){
+      min = array[i];
+    }
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined){
+    return 0;
+  }
+  let len = array.length;
+  if(len == 0){
+    return 0;
+  }
+  let max =  array[0];
+  for(let i = 1; i < len; i++){
+    if(array[i] > max){
+      max = array[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined){
+    return 0;
+  }
+  let len = array.length;
+  if(len == 0){
+    return 0;
+  }  
+  let avg = 0;
+  for(let i = 0; i < len; i++){
+    avg = avg + array[i];
+  }
+  return avg/len;
 }
